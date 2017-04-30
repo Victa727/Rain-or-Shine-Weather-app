0e8d17f754d3b8dad9552fab00e0649083069605
Victor Aguiluz <victa727@yahoo.com>
2017-04-29 20:48:27 -0700

    yo commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rainOrShineWeatherAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rainOrShineWeatherAppApp
+ */
+angular.module('rainOrShineWeatherAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rainOrShineWeatherAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rainOrShineWeatherAppApp
+ */
+angular.module('rainOrShineWeatherAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
